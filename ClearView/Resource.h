9e8ffb0f4073f0a5cb955d4401b935c57b8ebd4e
Martijn van Antwerpen <tijn@tyndomyn.net>
2022-06-01 20:08:51 +0200

    Changed auto-increment values

--- ClearView/Resource.h
+++ ClearView/Resource.h
@@ -53,8 +53,8 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_RESOURCE_VALUE        123
+#define _APS_NEXT_COMMAND_VALUE         40017
 #define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
